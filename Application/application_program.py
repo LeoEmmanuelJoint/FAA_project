81d921fa6f4198039c35285b5f3a5d412c8cfe5d
brondonstyve <49815608+brondonstyve@users.noreply.github.com>
2023-06-26 01:47:39 -0400

    Update application_program.py

--- Application/application_program.py
+++ Application/application_program.py
@@ -1,10 +1,10 @@
 ##
-# @file application_program.py
+# @file application_program2.py
 # Principal file containing the python program
 # @author Léo-Emmanuel JOINT
 # @date Juney 2023
 
-# @remark 
+# @remark
 # In this program, the names of the elements are idetified with prefixes and suffixes
 # - the Tkinter control variables are named with the suffix "_gui"
 # - the Tkinter.ttk.Combobox elements are identified with the prefix "cbb_"
@@ -20,6 +20,8 @@
 # The application may take some time to setup
 
 # Program that shows the GUI
+import cv2
+
 print("Importing resources, please wait...")
 import tkinter as tk
 import tkinter.ttk as ttk
@@ -33,6 +35,7 @@ from joblib import load
 from sklearn.decomposition import PCA
 from skimage.feature import hog
 import pickle
+
 print("Done")
 print("Setting the interface...")
 
@@ -43,10 +46,10 @@ MyWindow = tk.Tk()
 
 # Theme to use in the interface (sets a style for each gui element)
 # Not all themes are avaliable depending on the distribution (Windows, Linux or Mac os)
-s1 =ttk.Style()
-#print("Available themes:", s1.theme_names()) #('winnative', 'clam', 'alt', 'default', 'classic', 'vista', 'xpnative') on Windows 10
-#print("Theme in use:", s1.theme_use()) # vista
-s1.theme_use('clam') # Sets the theme
+s1 = ttk.Style()
+# print("Available themes:", s1.theme_names()) #('winnative', 'clam', 'alt', 'default', 'classic', 'vista', 'xpnative') on Windows 10
+# print("Theme in use:", s1.theme_use()) # vista
+s1.theme_use('clam')  # Sets the theme
 
 ##
 # Font for the species displayed (italic)
@@ -56,15 +59,15 @@ frameRate = 25
 # Paths to the models and their results by default
 
 pathDict = {
-    'pathModel_CNN': "../CNN/CNN_V10_87/FERmodel_CNN.json",
-    'pathModel_weights_CNN': "../CNN/CNN_V10_87/FERmodel_CNN.h5",
-    'pathRes_CNN': "../CNN/CNN_V10_87/history_dict_bin.pkl",
+    'pathModel_CNN': "FERmodel_CNN.json",
+    'pathModel_weights_CNN': "FERmodel_CNN.h5",
+    'pathRes_CNN': "history_dict_bin.pkl",
     'pathRes_SVM': "../SVM/history_dict_svm1_bin.pkl",
-    'pathRes_RFC': "../Random Forest/test_performance_metrics_bin.pkl",
+    'pathRes_RFC': "test_performance_metrics_bin.pkl",
     'pathModel_SVM': "../SVM/svm_model.pkl",
-    'pathModel_RFC': "../Random Forest/emotion_model.pkl",
-    'pathHaarCascade': "./haarcascade_frontalface_default.xml",
-    'pathModel_PCA': "../Random Forest/pca_model1.pkl"
+    'pathModel_RFC': "emotion_model.pkl",
+    'pathHaarCascade': "haarcascade_frontalface_default.xml",
+    'pathModel_PCA': "pca_model1.pkl"
 }
 
 emotionsDict = {
@@ -75,9 +78,6 @@ emotionsDict = {
     4: 'neutral',
     5: 'sad',
     6: 'surprised'}
-
-
-
 
 
 # CLASSES
@@ -113,18 +113,18 @@ class Processing:
         self.FERModel = None
         self.currentModelName = ""
 
-        self.pca = PCA(n_components=100)
+        self.pca = PCA(n_components=60)
 
     def getEmotion(self, emotionId):
         return self.dict_emotions[emotionId]
 
     def getModelName(self, modelId):
         return self.dict_modelNames[modelId]
-    
+
     def load_model(self, idModel):
 
         modelName = self.dict_modelNames[idModel]
-        print("Loading "+modelName+" model ...")
+        print("Loading " + modelName + " model ...")
 
         if idModel == 0:
             self.FERModel = model_from_json(open(self.pathModelCNN, "r").read())
@@ -143,77 +143,53 @@ class Processing:
 
         print("Model loaded")
 
-    def extract_features(image):
+    def extract_features(self, image):
         # Appliquer le descripteur HOG
         hog_features = hog(image, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False)
         return hog_features
 
-    def load_pca_model(self, pca_filename, features):
-        """try:
-            loaded_pca = pickle.load(open(pca_filename, 'rb'))
-        except FileNotFoundError:
-            print("PCA Model file not found, creating it with 60 components")
-            loaded_pca = None
-
-        if loaded_pca is None:
-            pca = PCA(n_components=60)
-            pca.fit(features)
-        else:
-            pca = loaded_pca
-        """
-        pca = PCA(n_components=60)
-        pca.fit(features)
-
-        return pca
-
     def detect_on_image_path(self, pathImage):
-
         print("Opening file...")
         image = cv.imread(pathImage)
         print("File opened")
         print("Beginning detection on image...")
         return self.detect_on_image(image)
 
-    # Étape 2 : Charger le modèle PCA à partir du fichier ou effectuer PCA avec 60 composantes si le fichier n'est pas trouvé
-    def load_pca_model(self, pca_filename, features):
+    def get_prediction_nb_RFC(self, face_img):
+        # Charger le modèle RandomForestClassifier à partir du fichier
+        model = pickle.load(open(self.pathModelRFC, 'rb'))
+
         try:
-            loaded_pca = pickle.load(open(pca_filename, 'rb'))
+            loaded_pca = pickle.load(open(self.pathPCA, 'rb'))
         except FileNotFoundError:
             loaded_pca = None
 
         if loaded_pca is None:
             from sklearn.decomposition import PCA
             pca = PCA(n_components=60)
-            pca.fit(features)
+            pca.fit(face_img)
         else:
             pca = loaded_pca
 
-        return pca
-    def preprocess_image_RFC(self, image):
-        # Charger l'image en niveaux de gris
-        #image = cv2.imread(image_path, 0)
-        image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-        # Redimensionner l'image
-        image = cv.resize(image, (48, 48))
-        return image
-    def extract_features(self, image):
-        # Appliquer le descripteur HOG
-        hog_features = hog(image, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False)
-        return hog_features
-    def get_prediction_nb_RFC(self, face_img):
-
         # Prétraiter l'image
-        #image = self.preprocess_image_RFC(face_img)
-        # Extraire les caractéristiques (HOG) de l'image
-        features = self.extract_features(face_img)
-        # Charger le modèle PCA à partir du fichier ou effectuer PCA avec 60 composantes
-        pca = self.load_pca_model(self.pathPCA, [features])
-        # Réduire les caractéristiques de l'image en utilisant les composantes principales de PCA
-        features_pca = pca.transform([features])
-        # Prédire l'émotion
-        emotion_index = self.FERModel.predict(features_pca)[0]
+        resized_image = cv2.resize(face_img, (48, 48))
 
+        # Extraire les caractéristiques (HOG) de l'image
+        hog_features = hog(resized_image, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2),
+                           visualize=False)
+
+        # Réduire les caractéristiques de l'image en utilisant les composantes principales de PCA
+        features_pca = pca.transform([hog_features])
+
+        # Prédire l'émotion
+        emotion_index = model.predict(features_pca)[0]
+
+        emotion = emotionsDict[emotion_index]
+
+        # Retourner la prédiction sous forme d'entier
         return emotion_index
+
+
 
     def get_prediction_nb_CNN(self, face_img):
 
@@ -224,7 +200,7 @@ class Processing:
         # Predict the emotion
         predictions = self.FERModel.predict(input_image)
         best = np.argmax(predictions[0])
-        return(best)
+        return (best)
 
     def detect_on_image(self, image):
 
@@ -255,24 +231,24 @@ class Processing:
             # Draw rectangle around the face
             cv.rectangle(image_copy, (x, y), (x + w, y + h), (0, 255, 0), 2)
             # Write the corresponding emotion
-            cv.putText(image_copy, emotion_predicted, (int(x), int(y+h)), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            cv.putText(image_copy, emotion_predicted, (int(x), int(y + h)), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-        return(image_copy)
+        return (image_copy)
 
     def get_training_results(self):
         with open(self.pathModelResults, 'rb') as f:
-            #f.seek(0)
+            # f.seek(0)
             self.dict_res = pickle.load(f)
         conf_matrix = self.dict_res["test_cf_matrix"]
         conf_matrix = np.array2string(conf_matrix, precision=2, separator=' ', suppress_small=True)
         accuracy = self.dict_res["test_accuracy"]
         kappa = self.dict_res["test_kappa"]
         report = self.dict_res["test_report"]
-        res = "Accuracy: "+str(accuracy)+ \
+        res = "Accuracy: " + str(accuracy) + \
               "\n\nKappa: " + str(kappa) + \
-              "\n\nClassification report: \n"+report+"\n\nConfusion matrix: \n"+conf_matrix
+              "\n\nClassification report: \n" + report + "\n\nConfusion matrix: \n" + conf_matrix
         print(res)
-        return(res)
+        return (res)
 
 
 class Interface(ttk.Frame):
@@ -282,7 +258,7 @@ class Interface(ttk.Frame):
         ttk.Frame.__init__(self, master=mainframe)
         self.pack(side='top', expand=True, fill='both')
         self.master.title("Face Emotion Recognition test program")
-        self.frameDuration = int(1000/frameRate)
+        self.frameDuration = int(1000 / frameRate)
         self.monoFont = ('Courier', 9)
 
         self.defaultTestResultText = "Please select a model."
@@ -304,11 +280,11 @@ class Interface(ttk.Frame):
         self.lbf_modelSelection.pack(side='top', expand=False, fill='both', anchor='w', padx=3, pady=2)
 
         self.rbtn_CNN = ttk.Radiobutton(self.lbf_modelSelection, text="CNN", variable=self.modelId_gui, value=0,
-            cursor="hand2")
+                                        cursor="hand2")
         self.rbtn_SVM = ttk.Radiobutton(self.lbf_modelSelection, text="SVM", variable=self.modelId_gui, value=1,
-            cursor="hand2")
+                                        cursor="hand2")
         self.rbtn_RFC = ttk.Radiobutton(self.lbf_modelSelection, text="RFC", variable=self.modelId_gui, value=2,
-            cursor="hand2")
+                                        cursor="hand2")
 
         self.rbtn_CNN.grid(row=0, column=0, sticky='nw')
         self.rbtn_SVM.grid(row=1, column=0, sticky='nw')
@@ -320,13 +296,15 @@ class Interface(ttk.Frame):
         self.lbf_actions.pack(side="top", expand=False, fill='both')
 
         # See performances
-        #self.btn_seePerf = ttk.Button(self.lbf_actions, command=self.updateTestResults, text="See performances", cursor="hand2")
-        #self.btn_seePerf.pack(side="left", expand=True, fill='both')
+        # self.btn_seePerf = ttk.Button(self.lbf_actions, command=self.updateTestResults, text="See performances", cursor="hand2")
+        # self.btn_seePerf.pack(side="left", expand=True, fill='both')
         # Execute the model on a given image
-        self.btn_runOnImage = ttk.Button(self.lbf_actions, command=self.detect_on_image, text="Run model on image", cursor="hand2")
+        self.btn_runOnImage = ttk.Button(self.lbf_actions, command=self.detect_on_image, text="Run model on image",
+                                         cursor="hand2")
         self.btn_runOnImage.pack(side="left", expand=True, fill='both')
         # Execute the model on the webcam
-        self.btn_runOnVideo = ttk.Button(self.lbf_actions, command=self.detect_on_videoStream, text="Run model on webcam", cursor="hand2")
+        self.btn_runOnVideo = ttk.Button(self.lbf_actions, command=self.detect_on_videoStream,
+                                         text="Run model on webcam", cursor="hand2")
         self.btn_runOnVideo.pack(side="left", expand=True, fill='both')
 
         # Part with the display
@@ -350,7 +328,7 @@ class Interface(ttk.Frame):
 
         self.modelId_gui.trace_add(mode="write", callback=self.onSelectModel)
 
-        self.modelId_gui.set(0) # Initialisation à l'affichage du CNN
+        self.modelId_gui.set(0)  # Initialisation à l'affichage du CNN
 
     def onSelectModel(self, *args):
         self.close_videoStream()
@@ -377,7 +355,7 @@ class Interface(ttk.Frame):
         relativePathToImage = os.path.relpath(pathToImage)
         print("Opening image on path: ", relativePathToImage)
 
-        if len(relativePathToImage) > 0 :
+        if len(relativePathToImage) > 0:
             image = self.process.detect_on_image_path(relativePathToImage)
 
         self.show_image(image)
@@ -399,7 +377,6 @@ class Interface(ttk.Frame):
             print("Video stream closed")
         self.canvas1.delete('all')
 
-
     def detect_on_videoStream(self, streamSourceId=0):
 
         print("Getting stream source...")
@@ -407,7 +384,6 @@ class Interface(ttk.Frame):
         self.loopOnVideoStream = True
         print("Starting loop")
         self.updateFrames()
-
 
     def clearDisplays(self):
         self.lbl_result.configure(text=self.defaultTestResultText)
@@ -422,18 +398,18 @@ class Interface(ttk.Frame):
         # images in RGB order, so we need to swap the channels
         image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
         img_h, img_w = image.shape[0:2]
-        ratioWH = img_w/img_h
+        ratioWH = img_w / img_h
         w = self.canvas1.winfo_width()
         h = self.canvas1.winfo_height()
 
-        if w/h > 1:
+        if w / h > 1:
             new_img_h = h
-            new_img_w = int(h*ratioWH)
+            new_img_w = int(h * ratioWH)
         else:
-            new_img_h = int(w/ratioWH)
+            new_img_h = int(w / ratioWH)
             new_img_w = w
 
-        image = cv.resize(image, (new_img_w, new_img_h), interpolation = cv.INTER_AREA)
+        image = cv.resize(image, (new_img_w, new_img_h), interpolation=cv.INTER_AREA)
 
         # convert the images to PIL format...
         image = Image.fromarray(image)
@@ -447,7 +423,6 @@ class Interface(ttk.Frame):
         cv.imshow('Result', image)
         cv.waitKey(0)
         """
-
 
 
 # MAIN PROGRAM
